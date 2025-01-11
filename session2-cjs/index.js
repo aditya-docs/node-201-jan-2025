391eb441bfcb05c4e7e777ad78a27eec84dee6fc
Aditya Singh <106972852+aditya-docs@users.noreply.github.com>
2025-01-11 17:03:25 +0000

    session2
    /users routes

--- session2-cjs/index.js
+++ session2-cjs/index.js
@@ -3,6 +3,11 @@ const {
   getCurrencies,
   getCurrencyBySymbol,
 } = require("./controllers/currencies.controllers");
+const {
+  getUsers,
+  getUserById,
+  searchUsers,
+} = require("./controllers/users.controllers");
 const app = express();
 const PORT = 8082;
 
@@ -14,9 +19,11 @@ app.get("/currencies", getCurrencies);
 
 app.get("/currencies/:symbol", getCurrencyBySymbol);
 
-// app.get("/posts/:postId/comments/:commentId", (req, res) => {
-//   console.log(req.params);
-// });
+app.get("/users", getUsers);
+
+app.get("/users/search", searchUsers);
+
+app.get("/users/:uuid", getUserById);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
