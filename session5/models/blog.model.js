193de533f5c7ee77d0e9016f22d350b50c8ed512
Aditya Singh <106972852+aditya-docs@users.noreply.github.com>
2025-01-16 16:26:06 +0000

    session5
    timestamps in schema

--- session5/models/blog.model.js
+++ session5/models/blog.model.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: { type: String, required: true, unique: true },
-  author: { type: [String], default: [] },
-  content: { type: String, default: "" },
-  publishedAt: { type: Date, default: null },
-});
+const blogSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    author: { type: [String], default: [] },
+    content: { type: String, default: "" },
+    publishedAt: { type: Date, default: null },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
 const blogModel = mongoose.model("Blog", blogSchema);
 
